7219595c8709cd0084e7b21630c856e48df8ff5d
ChrisCoolman <159795512+ChrisCoolman@users.noreply.github.com>
2025-06-03 08:39:57 -0400

    changed how the random works

--- shuffle.cpp
+++ shuffle.cpp
@@ -12,8 +12,8 @@ void shuffle(std::string playlistFilePath) {
     std::vector<std::string> row;
 
     std::vector<std::string> songNames;
-    std::vector<std::string> ArtistNames;
-    std::vector<std::string> AlbumNames;
+    std::vector<std::string> artistNames;
+    std::vector<std::string> albumNames;
 
     // read file
     std::ifstream file(playlistFilePath);
@@ -31,41 +31,61 @@ void shuffle(std::string playlistFilePath) {
 
     for(int i = 0; i < row.size(); i+=3) {
         songNames.push_back(row[i]);
-        ArtistNames.push_back(row[i + 1]);
-        AlbumNames.push_back(row[i + 2]);
+        artistNames.push_back(row[i + 1]);
+        albumNames.push_back(row[i + 2]);
     }
     /* print the songs
     for(int i = 0; i < songNames.size(); i+=1) {
         std::cout << songNames[i] << std::endl;
     }
 
-    for(int i = 0; i < ArtistNames.size(); i+=1) {
-        std::cout << ArtistNames[i] << std::endl;
+    for(int i = 0; i < artistNames.size(); i+=1) {
+        std::cout << artistNames[i] << std::endl;
     }
 
-    for(int i = 0; i < AlbumNames.size(); i+=1) {
-        std::cout << AlbumNames[i] << std::endl;
+    for(int i = 0; i < albumNames.size(); i+=1) {
+        std::cout << albumNames[i] << std::endl;
     }
     */
     int min = 0;
     int max = songNames.size() - 1;
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    int randomNumber;
-    num = -1;
-    
-    for(int i = 0; i < songNames.size(); i+=1) {
-        while(num > -1 && ArtistNames[num] != ArtistNames[i]) {
+    num = 0;
+    bool cool = true;
+    for(int i = 0; i < songNames.size(); i++) {
+        while(cool) {
+            std::random_device rd;
+            std::mt19937 gen(rd());
             std::uniform_int_distribution<> distrib(min, max);
             num = distrib(gen);
-            std::string temp = songNames[i];
-            songNames.erase(songNames.begin() + i);
-            songNames.insert(songNames.begin() + num, temp);
+            bool sameArtist = true;
+            bool sameAlbum = true;
+            if(artistNames[i] != artistNames[num] && artistNames[i] != artistNames[num - 1] && artistNames[i] != artistNames[num + 1]) {
+                sameArtist = false;
+            }
+            if(albumNames[i] != albumNames[num] && albumNames[i] != albumNames[num - 1] && albumNames[i] != albumNames[num + 1]) {
+                sameAlbum = false;
+            }
+            std::cout << sameArtist << " <- artist - album -> " << sameAlbum << std::endl;
+            if(sameArtist == false && sameAlbum == false) {
+                std::string temp = songNames[i];
+                std::string replacement = songNames[num];
+                songNames.erase(songNames.begin() + i);
+                songNames.insert(songNames.begin() + i, replacement);
+                songNames.insert(songNames.begin() + num, temp);
+                temp = artistNames[i];
+                replacement = artistNames[num];
+                artistNames.erase(artistNames.begin() + i);
+                artistNames.insert(artistNames.begin() + i, replacement);
+                artistNames.insert(artistNames.begin() + num, temp);
+                cool = false;
+            }
+            else {
+                continue;
+            }
         }
     }
-    for(int i = 0; i < songNames.size(); i+=1) {
-        std::cout << songNames[i] << std::endl;
+    for(int i = 0; i < songNames.size(); i++) {
+        std::cout << songNames[i] << " - " << artistNames[i] << std::endl;
     }
-
     file.close();
 }
