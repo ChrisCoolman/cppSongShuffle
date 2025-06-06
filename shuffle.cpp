83ce81eb42d5b6176907a4f4c09b003c9cda42e1
ChrisCoolman <159795512+ChrisCoolman@users.noreply.github.com>
2025-06-06 08:37:57 -0400

    started adding feature support
    It can hopefully detect features and extract them into seperate artists

--- shuffle.cpp
+++ shuffle.cpp
@@ -40,33 +40,44 @@ void shuffle(std::string playlistFilePath)
         artistNames.push_back(row[i + 1]);
         albumNames.push_back(row[i + 2]);
     }
-    /* print the songs
-    for(int i = 0; i < songNames.size(); i+=1) {
-        std::cout << songNames[i] << std::endl;
-    }
 
-    for(int i = 0; i < artistNames.size(); i+=1) {
-        std::cout << artistNames[i] << std::endl;
-    }
-
-    for(int i = 0; i < albumNames.size(); i+=1) {
-        std::cout << albumNames[i] << std::endl;
-    }
-    */
     num = 0;
     std::random_device rd;
     std::mt19937 gen(rd());
     std::vector<std::string> NEWsongNames;
     std::vector<std::string> NEWartistNames;
     std::vector<std::string> NEWalbumNames;
+    std::vector<std::string> features;
+    std::vector<int> charpos;
     bool loopVar = true;
     for (int i = 0; i < songNames.size(); i < 0)
     {
         loopVar = true;
+        features.clear();
+        charpos.clear();
         while (loopVar)
         {
             std::uniform_int_distribution<> distrib(0, songNames.size() - 1);
             num = distrib(gen);
+            for(int i = 0; i < artistNames.size(); i++) {
+                if(artistNames[i] == "&") {
+                    charpos.push_back(i);
+                }
+            }
+            for(int i = 0; i < charpos.size(); i++) {
+                if(i == 0) {
+                    // take from the start to the first feature
+                    features.push_back(artistNames[i].substr(0, charpos[i]));
+                }
+                else if(i == charpos.size() - 1) {
+                    // take from the current feature to the end
+                    features.push_back(artistNames[i].substr(charpos[i], (charpos.size() - charpos[i])));
+                }
+                else {
+                    // take from the last feature to this feature
+                    features.push_back(artistNames[i].substr(charpos[i], charpos[i + 1]));
+                }
+            }
             if (NEWsongNames.size() > 0)
             {
                 if (artistNames[num] != NEWartistNames[i])
@@ -97,6 +108,11 @@ void shuffle(std::string playlistFilePath)
     for (int i = 0; i < NEWsongNames.size(); i += 1)
     {
         std::cout << i << ". " << NEWsongNames[i] << " - " << NEWartistNames[i] << std::endl;
+    }
+
+    for (int i = 0; i < charpos.size(); i += 1)
+    {
+        std::cout << charpos[i] << std::endl;
     }
     file.close();
 }
